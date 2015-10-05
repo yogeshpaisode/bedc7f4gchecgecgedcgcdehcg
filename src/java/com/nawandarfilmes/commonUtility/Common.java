06e9f2d63318d7acc5a8fdfb074137dfe2602856
yogesh <yogesh@192.168.1.7>
2015-10-05 17:50:24 +0530

    Test

--- src/java/com/nawandarfilmes/commonUtility/Common.java
+++ src/java/com/nawandarfilmes/commonUtility/Common.java
@@ -3,10 +3,13 @@
  * and open the template in the editor.
  */
 package com.nawandarfilmes.commonUtility;
+
 import com.nawandarfilmes.Hibernate.*;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
@@ -18,10 +21,44 @@ import org.hibernate.criterion.Restrictions;
 public class Common {
 
     private double mg_net_amount = 0;
-    private boolean flag=true;
-   
+    private boolean flag = true;
+    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    private Date end_Date = null;
+    Calendar c = Calendar.getInstance();
+
     public int getDays(Date to, Date from) {
         return (int) ((to.getTime() - from.getTime()) / (1000 * 60 * 60 * 24)) + 1;
+    }
+
+    public int getMovie_ID() {
+        int mov_id = 0;
+        Session session = sessionFactory.openSession();
+        Criteria criteria = session.createCriteria(MovieDetail.class);
+        for (Object o : criteria.list()) {
+            MovieDetail md = (MovieDetail) o;
+            mov_id = md.getMovId();
+        }
+        session.close();
+        return mov_id;
+    }
+
+    public Date get_Last_End_Date(int mov_id) {
+        c.set(2014, 1, 1);
+        end_Date = c.getTime();
+        Session session = sessionFactory.openSession();
+        Criteria criteria = session.createCriteria(MovieDetail.class);
+        criteria.add(Restrictions.eq("movId", mov_id));
+        MovieDetail md = (MovieDetail) criteria.list().get(0);
+        criteria = session.createCriteria(WorkOrder.class);
+        criteria.add(Restrictions.eq("movieDetail", md));
+        for (Object o : criteria.list()) {
+            WorkOrder wo = (WorkOrder) o;
+            if (wo.getWoEndDate().after(end_Date)) {
+                end_Date = wo.getWoEndDate();
+            }
+        }
+        session.close();
+        return end_Date;
     }
 
     public String getZeros(int id) {
@@ -45,11 +82,13 @@ public class Common {
         SimpleDateFormat sdf = new SimpleDateFormat("EEE");
         return sdf.format(date);
     }
+
     public String formateDate(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
         return sdf.format(date);
     }
-     public String formateDate_yyyy_MM_dd(Date date) {
+
+    public String formateDate_yyyy_MM_dd(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return sdf.format(date);
     }
@@ -97,9 +136,9 @@ public class Common {
         double amount = 0;
         mg_net_amount = mg_net_amount + nett_Amount;
         if (mg_net_amount > mg_amount) {
-            if(flag){
-                nett_Amount=mg_net_amount-mg_amount;
-                flag=false;
+            if (flag) {
+                nett_Amount = mg_net_amount - mg_amount;
+                flag = false;
             }
             double rent = total_rent;
             if (per_day_flag == true) {
@@ -124,32 +163,32 @@ public class Common {
         if (share_flag) {
             amount = getDistributerShare(dis_Share, total_rent, nett_Amount, per_day_flag, days);
         }
-        if(mg_flag){
-            amount=getDistributerMG(dis_Share, total_rent, nett_Amount, per_day_flag, days, mg_amount);
+        if (mg_flag) {
+            amount = getDistributerMG(dis_Share, total_rent, nett_Amount, per_day_flag, days, mg_amount);
         }
         return amount;
     }
-    public double getPaymentReceived(WorkOrder wo){
-        double amount=0;
-        SessionFactory sessionFactory=HibernateUtil.getSessionFactory();
-        Session session=sessionFactory.openSession();
-        Criteria cash_criteria=session.createCriteria(CashPayment.class);
+
+    public double getPaymentReceived(WorkOrder wo) {
+        double amount = 0;
+        Session session = sessionFactory.openSession();
+        Criteria cash_criteria = session.createCriteria(CashPayment.class);
         cash_criteria.add(Restrictions.eq("workOrder", wo));
-        for(Object cash_obj:cash_criteria.list()){
-            CashPayment cp=(CashPayment) cash_obj;
-            amount=amount+cp.getAmtRecv();
+        for (Object cash_obj : cash_criteria.list()) {
+            CashPayment cp = (CashPayment) cash_obj;
+            amount = amount + cp.getAmtRecv();
         }
-        Criteria nr_criteria=session.createCriteria(NeftRtgsPayment.class);
+        Criteria nr_criteria = session.createCriteria(NeftRtgsPayment.class);
         nr_criteria.add(Restrictions.eq("workOrder", wo));
-        for(Object nr_obj:nr_criteria.list()){
-            NeftRtgsPayment nr=(NeftRtgsPayment) nr_obj;
-            amount=amount+nr.getAmtRecv();
+        for (Object nr_obj : nr_criteria.list()) {
+            NeftRtgsPayment nr = (NeftRtgsPayment) nr_obj;
+            amount = amount + nr.getAmtRecv();
         }
-        Criteria cheque_criteria=session.createCriteria(CheaquePayment.class);
+        Criteria cheque_criteria = session.createCriteria(CheaquePayment.class);
         cheque_criteria.add(Restrictions.eq("workOrder", wo));
-        for(Object cheque_obj:cheque_criteria.list()){
-            CheaquePayment cp=(CheaquePayment) cheque_obj;
-            amount=amount+cp.getAmtRecv();
+        for (Object cheque_obj : cheque_criteria.list()) {
+            CheaquePayment cp = (CheaquePayment) cheque_obj;
+            amount = amount + cp.getAmtRecv();
         }
         session.close();
         return amount;
